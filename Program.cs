adcc76316729381d994353e3e97105cbc40e4ecc
Fernando-Castro-Hernandez <jesus.castro@tecdesoftware.edu.mx>
2026-05-12 13:08:44 -0600

    refactor: limpiar Program.cs con inyección de dependencias

--- Program.cs
+++ Program.cs
@@ -1,5 +1,32 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+﻿var repositorio = new Ahorcado.PalabrasEnMemoria();
+var motor = new Ahorcado.MotorAhorcado(repositorio);
+var ui = new Ahorcado.ConsolaUI(motor);
 
+Console.WriteLine("===AHORCADO===");
 
-var juego = new Ahorcado.Juego();
-juego.Jugar();
+while (!motor.Ganado() && !motor.Perdido())
+{
+    ui.MostrarTablero();
+
+    char letra = ui.PedirLetra();
+
+    if (motor.LetraYaUsada(letra))
+    {
+        ui.MostrarMensaje("Ya usaste esa letra.");
+        continue;
+    }
+
+    motor.RegistrarLetra(letra);
+}
+ui.MostrarTablero();
+
+if (motor.Ganado())
+    ui.MostrarMensaje($"\n¡Ganaste! La palabra era: {motor.PalabraSecreta}");
+else
+    ui.MostrarMensaje($"\nPerdiste. La palabra era: {motor.PalabraSecreta}");
+
+if (ui.PreguntarOtraVez())
+{
+    var nuevoMotor = new Ahorcado.MotorAhorcado(repositorio);
+    var nuevaUI = new Ahorcado.ConsolaUI(nuevoMotor);
+}
